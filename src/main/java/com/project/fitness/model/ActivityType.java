2ae86958fe8a579bc802bd29e1ca31e9f85cf436
Pallavi Pal <palpallavi523@gmail.com>
2026-08-26 23:13:20 +0530

    Add activity registration API

---
+++ src/main/java/com/project/fitness/model/ActivityType.java
@@ -0,0 +1,14 @@
+package com.project.fitness.model;
+
+import org.springframework.boot.SpringApplication;
+
+public enum ActivityType {
+    RUNNING,
+    WALKING,
+    CYCLING,
+    WEIGHT_TRAINING,
+    YOGA,
+    CARDIO,
+    STRETCHING,
+    OTHER
+}
